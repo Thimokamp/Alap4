b08a39ba2181685bfc466fb171e7905640fb23a6
Thimo kamp <150125005+Thimokamp@users.noreply.github.com>
2024-06-26 09:02:38 +0200

    Update Cookies.php

--- Cookies.php
+++ Cookies.php
@@ -1,18 +1,4 @@
-<?php
-if(!isset($_COOKIE[$cookie_name])) {
-  echo "Cookie named '" . $cookie_name . "' is not set!";
-} else {
-  echo "Cookie '" . $cookie_name . "' is set!<br>";
-  echo "Value is: " . $_COOKIE[$cookie_name];
-}
 
-function setcookies($username){
-  $cookie_name ="user";
-  $cookie_value = $username;
-  setcookie($cookie_name,$cookie_value,time() + (86400 *40))
-
-}
-?>
 
 
 
