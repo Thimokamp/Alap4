d6f1223575a3d72b5d4699e03e2b7cd2688b7fa2
Thimo kamp <150125005+Thimokamp@users.noreply.github.com>
2024-06-26 09:10:25 +0200

    Update login.php

--- login.php
+++ login.php
@@ -1,19 +1,83 @@
-<?php
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    // Define your username and password
-    $username = "jayjay";
-    $password = "123456";
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="author" >
+    <meta name="viewport" content="width=device-width,initial-scale=1">
+    <title>My Login Page</title>
+    <link rel="stylesheet" href="../css/style1.css">
+   
+</head>
+ 
+<body class="my-login-page">
+   
+            <div class="firstContainer">
+ 
+                <!--card-->
+                <div class="card">
+                   
+                    <div class="container">
+                      <h4><b>Login</b></h4>
+                     
+                   
+                           
+                            <!--card-inhoud-->
+                       
+                            <form action="../php/proces.php" method="post">
+                                <div class="form-group">
+                                <form method="post" action="../php/proces.php">
+                                    <div class="form-group">
+                                       
+                                            <label for="email"> E-mail</label>
+                                            <input id="email" type="text" class="form-control" name="email" required><br>
+                                        <br>
+                                    </div>  
+                                    <div class="form-group">
+                                        <label for="wachtwoord">Wachtwoord:</label>
+                                        <input id="wachtwoord" type="password" class="form-control" name="wachtwoord" required><br>
+                                        
+                                    </div>
 
-    // Check if the form's username and password matches
-    if ($_POST['username'] == $username && $_POST['password'] == $password) {
-        echo "Login successful!";
-        // Redirect or display a success message
-        header("Location: ../index.php"); // navigeren naar mijn index.html page
-        exit; 
-        
-    } else {
-        echo "Invalid username or password!";
-        // krijg error message
-    }
-}
-?>
+                                    <!--inloggegevens opslaan-->
+
+                                    <div class="form-group">
+                                        <div class="custom-checkbox custom-control">
+                                            <input type="checkbox" name="remember" id="remember" class="custom-control-input">
+                                            <label for="remember" class="custom-control-label">Remember Me</label>
+                                        </div>
+                                    </div>
+                                    <br>
+
+                                    <!--login-button-->
+
+                                    <div class="form-group m-0">
+                                        <button type="submit" name="login" value="login" class="btn btn-primary btn-block">
+                                           Login
+                                        </button>
+                                    </div>
+                                    
+                                    <!--account aanmaken-->
+
+                                    <div class="mt-4 text-center">
+                                        Don't have an account? <a href="../html/sign.html">Create One</a>
+                                    </div>
+                                 </form>
+                                </div>
+                                        
+                    
+                                    
+                                </div>
+ 
+                               
+                               
+                               
+                            </div>
+                        </div>
+                            </form>
+                        
+                   
+        </div>
+    </section>
+
+</body>
+</html>
